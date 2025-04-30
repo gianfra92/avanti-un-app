f131e5d50b6bce4c65726cfbb27b6beac0092a03
gianfra92 <gianfra92@gmail.com>
2025-04-30 13:10:36 +0200

    added android build

---
+++ android/app/src/main/java/com/avantiunapp/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.avantiunapp.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.avantiunapp.app',
+  appName: "Avanti Un'App",
+  webDir: 'dist-mobile/avanti-un-app/browser'
+};
+
+export default config;
